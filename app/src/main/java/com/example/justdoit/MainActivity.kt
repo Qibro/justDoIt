9dabe9a49de1d67ef070f062d3aed38e8112d2c1
Qibro <muhamad.ibrahim21@gmail.com>
2019-11-10 16:11:49 +0700

    Add Bottom Navigation View with Fragment

--- app/src/main/java/com/example/justdoit/MainActivity.kt
+++ app/src/main/java/com/example/justdoit/MainActivity.kt
@@ -3,18 +3,68 @@ package com.example.justdoit
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.fragment_profile.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var habitsFragment:HabitsFragment
+    lateinit var dailyFragment: DailyFragment
+    lateinit var todoFragment: TodoFragment
+    lateinit var profileFragment: ProfileFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn_sign_out.setOnClickListener(){
-            FirebaseAuth.getInstance().signOut()
-            startActivity(Intent(this,LoginActivity::class.java))
-            finish()
+        val bottomNav : BottomNavigationView = findViewById(R.id.bottom_nav)
+
+            habitsFragment = HabitsFragment()
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.frame_layout,habitsFragment)
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                .commit()
+
+        bottomNav.setOnNavigationItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.fragment_habits ->{
+                    habitsFragment = HabitsFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout,habitsFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+
+                R.id.fragment_daily ->{
+                    dailyFragment = DailyFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout,dailyFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+
+                R.id.fragment_todo ->{
+                    todoFragment = TodoFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout,todoFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+
+                R.id.fragment_profile ->{
+                    profileFragment = ProfileFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout,profileFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+            }
+            true
         }
     }
 }
---
+++ app/src/main/java/com/example/justdoit/ProfileFragment.kt
@@ -0,0 +1,38 @@
+package com.example.justdoit
+
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_profile.*
+import kotlinx.android.synthetic.main.fragment_profile.view.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class ProfileFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view: View = inflater.inflate(R.layout.fragment_profile, container, false)
+
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_sign_out.setOnClickListener{
+            FirebaseAuth.getInstance().signOut()
+            startActivity(Intent(activity?.applicationContext, LoginActivity::class.java))
+        }
+    }
+
+}
+
